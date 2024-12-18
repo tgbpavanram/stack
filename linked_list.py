e7310e862d82f98986276d03dfad5987345ad5cc
tgbpavanram <pavanram9063@gmail.com>
2024-12-18 21:40:50 +0530

    DSA

---
+++ linked_list.py
@@ -0,0 +1,32 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+# Example usage
+ll = LinkedList()
+ll.insert_at_end(20)
+ll.insert_at_end(30)
+ll.display()
